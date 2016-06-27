333e5c54c81057811a148cbab48dadbeede53060
Akira Matsuda <ronnie@dio.jp>
2016-06-28 04:38:24 +0900

    rails g controller posts show

---
+++ test/dummy_app/app/controllers/posts_controller.rb
@@ -0,0 +1,4 @@
+class PostsController < ApplicationController
+  def show
+  end
+end
--- test/dummy_app/config/routes.rb
+++ test/dummy_app/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'posts/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
