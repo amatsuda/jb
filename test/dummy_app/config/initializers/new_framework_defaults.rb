b1dffc5a45170da0badb60efd0bfd92280d6de2f
Akira Matsuda <ronnie@dio.jp>
2016-06-28 14:06:59 +0900

    compat

--- test/dummy_app/config/initializers/new_framework_defaults.rb
+++ test/dummy_app/config/initializers/new_framework_defaults.rb
@@ -4,11 +4,5 @@
 #
 # Read the Rails 5.0 release notes for more info on each option.
 
-# Enable per-form CSRF tokens. Previous versions had false.
-Rails.application.config.action_controller.per_form_csrf_tokens = true
-
-# Enable origin-checking CSRF mitigation. Previous versions had false.
-Rails.application.config.action_controller.forgery_protection_origin_check = true
-
 # Configure SSL options to enable HSTS with subdomains. Previous versions had false.
 Rails.application.config.ssl_options = { hsts: { subdomains: true } }
