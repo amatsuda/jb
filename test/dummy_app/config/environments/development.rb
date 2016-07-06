aa4f7df7c6b726e596fabde520542ccc10124ed1
Akira Matsuda <ronnie@dio.jp>
2016-07-06 14:16:51 +0900

    We're not using AR and assets in the tests

--- test/dummy_app/config/environments/development.rb
+++ test/dummy_app/config/environments/development.rb
@@ -29,17 +29,6 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
-
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
