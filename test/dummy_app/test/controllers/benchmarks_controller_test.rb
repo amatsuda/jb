0cd86cd4df299ee2fe7493c4d21760b72d2d534e
Akira Matsuda <ronnie@dio.jp>
2016-07-06 14:29:26 +0900

    :put_litter_in_its_place:

--- test/dummy_app/test/controllers/benchmarks_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BenchmarksControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get benchmarks_index_url
-    assert_response :success
-  end
-
-end
