01e778b14a5d5f91809aee1191e85630bb5301ec
Toru KAWAMURA <tkawa@4bit.net>
2017-01-26 18:43:14 +0900

    Support layouts

--- lib/jb/action_view_monkeys.rb
+++ lib/jb/action_view_monkeys.rb
@@ -40,11 +40,44 @@ module Jb
     end
   end
 
+  # An alternative OutputFlow that holds each content in Hash/Array as-is
+  class RawOutputFlow
+    attr_reader :content
+
+    def initialize
+      @content = Hash.new
+    end
+
+    def get(key)
+      @content[key]
+    end
+
+    def set(key, value)
+      @content[key] = value.extend(Jb::PartialRenderer::StrongArray)
+    end
+
+    def append(key, value)
+      if @content[key].nil?
+        set(key, value)
+      elsif @content[key].respond_to?(:merge)
+        @content[key].merge(value)
+      else
+        @content[key].concat(value)
+      end
+    end
+    alias_method :append!, :append
+  end
+
   # A monkey-patch that converts non-partial result to a JSON String
   module TemplateRenderer
     module JSONizer
       def render_template(template, *)
-        template.respond_to?(:handler) && (template.handler == Jb::Handler) ? MultiJson.dump(super) : super
+        if template.respond_to?(:handler) && template.handler == Jb::Handler
+          @view.view_flow = RawOutputFlow.new unless @view.view_flow.is_a?(RawOutputFlow)
+          MultiJson.dump(super)
+        else
+          super
+        end
       end
     end
   end
