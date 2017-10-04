97a256b267221e900d16a8d3934ed8cd2d5ec4e5
Francesco Rodriguez <frodsan@protonmail.com>
2017-10-04 12:50:45 +0200

    Add test for ActionController::API support.

--- test/dummy_app/config/routes.rb
+++ test/dummy_app/config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
   end
 
   resources :benchmarks, only: :index
+
+  if Rails::VERSION::MAJOR >= 5
+    namespace :api do
+      resources :posts, only: :show
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
