fb4b0de6c8b96b8b018cc623644a89a7b40faa96
Akira Matsuda <ronnie@dio.jp>
2023-09-17 00:00:38 +0900

    Don't stringify return values from jb templates
    Rails 7.1 calls to_s against all templates' return value since rails/rails@ee68644c284aa7512d06b4b5514b1236c1b63f55,
    and this incompatibility broke jb templates that return non-String objects, e.g. Hash or Array.
    This monkey-patch redefines to_s method on such objects not to stringify themselves.
    
    fixes #43

--- lib/jb/action_view_monkeys.rb
+++ lib/jb/action_view_monkeys.rb
@@ -13,6 +13,19 @@ module Jb
     end
   end
 
+  # Rails 7.1+: A monkey-patch not to stringify rendered object from JB templates
+  module BaseToSCanceller
+    def _run(method, template, *, **)
+      val = super
+      if template.respond_to?(:handler) && (template.handler == Jb::Handler)
+        def val.to_s
+          self
+        end
+      end
+      val
+    end
+  end
+
   # Rails 6.1+: A monkey-patch for jb template collection result's `body` not to return a String but an Array
   module CollectionRendererExtension
     private def render_collection(_collection, _view, _path, template, _layout, _block)
@@ -45,6 +58,7 @@ module Jb
 end
 
 ::ActionView::TemplateRenderer.prepend ::Jb::TemplateRenderer::JSONizer
+::ActionView::Base.prepend ::Jb::BaseToSCanceller
 begin
   # ActionView::CollectionRenderer is a newly added class since 6.1
   ::ActionView::CollectionRenderer.prepend ::Jb::CollectionRendererExtension
