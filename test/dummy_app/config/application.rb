ba6d18295817ea725d350190474f2d8a3b5fad44
Akira Matsuda <ronnie@dio.jp>
2023-11-06 00:09:24 +0900

    Allow www.example.com on the test app

--- test/dummy_app/config/application.rb
+++ test/dummy_app/config/application.rb
@@ -14,5 +14,7 @@ module Dummy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.hosts << 'www.example.com'
   end
 end
